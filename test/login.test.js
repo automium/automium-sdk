3b11a7a3ab5e6dae7f2879f976fc0fb88bb12ed3
Federico Minzoni <fminzoni@enter.it>
2019-04-02 09:58:40 +0200

    test(login): add test login implementation

--- test/login.test.js
+++ test/login.test.js
@@ -1,8 +1,51 @@
+jest.mock("got"); // got is now a mock function
+
 import "@babel/polyfill";
+import got from "got";
 import { invokeLogin } from "../src/auth/login";
 
+beforeEach(() => {
+  // Cleaning up between tests
+  got.mockClear();
+});
+
+it("login succeded with the valid input", async () => {
+  got.mockReturnValue(Promise.resolve({ body: "TOKEN" }));
+
+  const result = await invokeLogin(
+    "http://AUTOMIUM_URL",
+    "username",
+    "password"
+  );
+
+  expect(got).toHaveBeenCalledTimes(1);
+  expect(got).toHaveBeenCalledWith("http://AUTOMIUM_URL/login", {
+    body: { password: "password", username: "username" },
+    encoding: "utf8",
+    json: true,
+    method: "POST",
+    timeout: 200
+  });
+  expect(result.status).toBe(true);
+  expect(result.result).toBe("TOKEN");
+});
+
 it("login fails with the wrong credential", async () => {
-  expect.assertions(1);
-  const result = await invokeLogin("TEST_ENDPOINT", "username", "password");
-  expect(result.status).toEqual(false);
+  got.mockReturnValue(Promise.reject("Invalid credentials"));
+
+  const result = await invokeLogin(
+    "http://AUTOMIUM_URL",
+    "username",
+    "password"
+  );
+
+  expect(got).toHaveBeenCalledTimes(1);
+  expect(got).toHaveBeenCalledWith("http://AUTOMIUM_URL/login", {
+    body: { password: "password", username: "username" },
+    encoding: "utf8",
+    json: true,
+    method: "POST",
+    timeout: 200
+  });
+  expect(result.status).toBe(false);
 });
